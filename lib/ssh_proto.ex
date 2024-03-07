0d9ba9cbb6b1067e8a62643bec597bdb12f7d77b
Ashlynn Anderson <github@pea.sh>
2024-03-06 19:49:00 -0500

    Initial commit

---
+++ lib/ssh_proto.ex
@@ -0,0 +1,18 @@
+defmodule SshProto do
+  @moduledoc """
+  Documentation for `SshProto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SshProto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ssh_proto_test.exs
@@ -0,0 +1,8 @@
+defmodule SshProtoTest do
+  use ExUnit.Case
+  doctest SshProto
+
+  test "greets the world" do
+    assert SshProto.hello() == :world
+  end
+end
