af97925b8875fce0c73323221c794cff6eba8ad0
Ashlynn Anderson <github@pea.sh>
2024-03-06 21:18:16 -0500

    Implement version message encode/decode

--- lib/ssh_proto.ex
+++ lib/ssh_proto.ex
@@ -55,4 +55,55 @@ defmodule SshProto do
   def encode(_state, _msg) do
     {:error, :unimplemented}
   end
+
+  @crlf "\u000d\u000a"
+
+  @doc """
+  Decode version message for SSH version exchange.
+
+  ## Examples
+
+      iex> SshProto.decode_version("SSH-2.0-OpenSSH_9.6\\r\\n")
+      {:ok, "OpenSSH_9.6"}
+
+      iex> SshProto.decode_version("nonsense")
+      {:error, {:parse_error, "\\"nonsense\\" is not a valid version message"}}
+
+  """
+  @spec decode_version(binary()) :: {:ok, String.t()} | {:error, any()}
+  def decode_version(msg) do
+    case String.split(msg, ["-", @crlf, " "], parts: 4) do
+      ["SSH", ssh_version, remote_version, _] ->
+	if ssh_version in ["2.0", "1.99"] do
+	  {:ok, remote_version}
+	else
+	  {:error, {:incompatible_remote_version, remote_version}}
+	end
+      _ ->
+	{:error, {:parse_error, "\"#{msg}\" is not a valid version message"}}
+    end
+  end
+
+  @doc """
+  Encode version message for SSH version exchange. `version_string` can only
+  contain printable US-ASCII characters, and CANNOT contain whitespace or
+  the minus sign (-).
+
+  ## Examples
+
+      iex> SshProto.encode_version("Stellar_0.1.0")
+      {:ok, "SSH-2.0-Stellar_0.1.0\\r\\n"}
+
+      iex> SshProto.encode_version("Stellar-0.1.0")
+      {:error, :illegal_version_string}
+
+  """
+  @spec encode_version(String.t()) :: {:ok, String.t()} | {:error, any()}
+  def encode_version(version_string) do
+    if SshProto.Util.legal_version_string?(version_string) do
+      {:ok, "SSH-2.0-" <> version_string <> @crlf}
+    else
+      {:error, :illegal_version_string}
+    end
+  end
 end
---
+++ lib/ssh_proto/util.ex
@@ -0,0 +1,22 @@
+defmodule SshProto.Util do
+  require Logger
+  @doc """
+  Checks if version string is legal. Version string can only contain printable
+  US-ASCII characters and cannot ' ' or '-'.
+
+  ## Examples
+
+      iex> SshProto.Util.legal_version_string?("Stellar_0.1.0")
+      true
+
+      iex> SshProto.Util.legal_version_string?("Stellar-0.1.0")
+      false
+
+  """
+  @spec legal_version_string?(String.t()) :: boolean()
+  def legal_version_string?(version_string) do
+    version_string
+    |> :binary.bin_to_list
+    |> Enum.all?(fn c -> c in 33..44 or c in 46..126 end)
+  end
+end
